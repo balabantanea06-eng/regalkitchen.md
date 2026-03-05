28379235ed4f50ea3d1f4d677eae5d8fbf75257f
balabantanea06-eng <balabantanea06@gmail.com>
2026-03-05 13:28:39 +0200

    Update error.vue

--- error.vue
+++ error.vue
@@ -1,30 +1,47 @@
 <template>
-  <Navbar/>
-  <Hero title="404"/>
+  <Navbar />
+  <Hero title="404" />
 
-          <!-- 404 Start -->
-          <div class="container-fluid py-6 wow fadeInUp" data-wow-delay="0.1s">
-            <div class="container text-center">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
-                        <h1 class="display-1">404</h1>
-                        <h1 class="mb-4">Page Not Found</h1>
-                        <p class="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
-                        <NuxtLink to="/"class="btn btn-primary rounded-pill py-3 px-5" >Go Back To Home </NuxtLink> 
-                    </div>
-                </div>
-            </div>
+  <!-- 404 Start -->
+  <div class="container-fluid py-6 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+
+          <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+
+          <h1 class="display-1">404</h1>
+
+          <h2 class="mb-4">Page Not Found</h2>
+
+          <p class="mb-4">
+            Sorry, the page you are looking for does not exist.
+            Please return to the home page.
+          </p>
+
+          <NuxtLink
+            to="/"
+            class="btn btn-primary rounded-pill py-3 px-5"
+          >
+            Go Back Home
+          </NuxtLink>
+
         </div>
-        <!-- 404 End -->
-  <Footer/>
-  <BackToTop/>
+      </div>
+    </div>
+  </div>
+  <!-- 404 End -->
+
+  <Footer />
+  <BackToTop />
 </template>
 
 <script lang="ts" setup>
-
+useHead({
+  title: "404 - Page Not Found"
+})
 </script>
 
-<style>
+<style scoped>
 
 </style>
